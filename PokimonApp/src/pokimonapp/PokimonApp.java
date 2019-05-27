32f2b1a64fd2960df27b277ee1185214db1b0b5f
alumno informatica <alumno@ciis.usil.edu.pe>
2019-05-27 12:42:13 -0500

    Version 1.1

--- PokimonApp/src/pokimonapp/PokimonApp.java
+++ PokimonApp/src/pokimonapp/PokimonApp.java
@@ -1,22 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pokimonapp;
 
-/**
- *
- * @author alumno
- */
+import interfaz.jfBienvenido;
+
 public class PokimonApp {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola perra");
+        jfBienvenido marco= new jfBienvenido();
+        marco.setVisible(true);
     }
     
 }

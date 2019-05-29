6be550df07d8facdcf06b7c4e9556b20b9a8c958
Edgard Cuellar <icedgard19701@outlook.es>
2019-05-29 11:44:44 -0500

    Avance

---
+++ PokimonApp/src/entidades/Entrenador.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author alumno
+ */
+public class Entrenador {
+    private String nombre;
+    private boolean sexo;
+
+    public Entrenador(String n, boolean sexo) {
+        this.nombre = n;
+        this.sexo = sexo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isSexo() {
+        return sexo;
+    }
+
+    public void setSexo(boolean sexo) {
+        this.sexo = sexo;
+    }
+    
+    
+    
+    
+    
+    
+}
